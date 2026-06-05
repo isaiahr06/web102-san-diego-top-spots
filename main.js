dd0d6c94bf14b2706c7f79269e77385265b8cd07
Isaiah Rodriguez <irod196@wgu.edu>
2026-06-05 16:44:39 -0500

    Fix spot table

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
     // write your code here
+$.getJSON("data.json", function(data) {
+    // process the JSON data
+    data.forEach(function(spot) {
+        let mapLink = `https://www.google.com/maps?q= ${spot.location[0]},${spot.location[1]}`;
 
+        let row = `<tr>
+        <td>${spot.name}</td>
+        <td>${spot.description}</td>
+        <td><a href="${mapLink}" target="_blank">View on Map</a></td>
+        </tr>`;
+
+        $("#spots-table").append(row);
+    });
+})
 });
