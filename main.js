3f4c165db8fa09b84918ecd4064deb35386c9cb6
Isaiah Rodriguez <irod196@wgu.edu>
2026-06-05 17:03:10 -0500

    Fixed title and mapLink

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 $.getJSON("data.json", function(data) {
     // process the JSON data
     data.forEach(function(spot) {
-        let mapLink = `https://www.google.com/maps?q= ${spot.location[0]},${spot.location[1]}`;
+        let mapLink = `https://www.google.com/maps?q=${spot.location[0]},${spot.location[1]}`;
 
         let row = `<tr>
         <td>${spot.name}</td>
@@ -12,7 +12,7 @@ $.getJSON("data.json", function(data) {
         </tr>`;
 
         $("#spots-table").append(row);
-        
+
     });
   });
 });
