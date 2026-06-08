05c5620d33f6409e59cf23de302960e554b074d1
Isaiah Rodriguez <irod196@wgu.edu>
2026-06-08 16:11:47 -0500

    Added all rows into cards that included title , decription, maplink,  and image for a more appealing visual.

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ $(document).ready(function () {
     // write your code here
 $.getJSON("data.json", function(data) {
     // process the JSON data
-    data.forEach(function(spot) {
-        let mapLink = `https://www.google.com/maps?q=${spot.location[0]},${spot.location[1]}`;
+    data.forEach(function(spot,index) {
+        let mapLink =`https://www.google.com/maps?q=${spot.location[0]},${spot.location[1]}`;
+
+        let card = `
+        <div class="card">
+            <img src="${spot.image}" alt="${spot.name}">
+            <div class="card-context">
+                <h2>#${index + 1} ${spot.name}</h2>
+                <p>${spot.description}</p>
+                <a href="${mapLink}" target="_blank" class="map-btn">View On Map</a>
+            </div>
+        </div>`
+
+        $("#spots-container").append(card);
 
         let row = `<tr>
         <td>${spot.name}</td>
